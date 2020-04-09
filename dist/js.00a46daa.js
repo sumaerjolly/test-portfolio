4240567bd375638ca786a9a437ffbdb50ab11382
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-09 18:19:53 +0530

    add basic home page styling and iamges

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
+},{"./../img/IMG_0382.JPG":[["IMG_0382.a442a03b.JPG","img/IMG_0382.JPG"],"img/IMG_0382.JPG"],"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/index.js":[function(require,module,exports) {
 "use strict";
 
 require("../scss/main.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50248" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53230" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
